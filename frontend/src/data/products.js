ce667328fa780176fe8f9cf6e13d8b414ed52650
Kairo Chácara <144383548+ChacaraKairo@users.noreply.github.com>
2026-07-02 13:34:43 -0300

    feat: normaliza produtos para estratégia fast-food

--- frontend/src/data/products.js
+++ frontend/src/data/products.js
@@ -1,3 +1,4 @@
 import { generatedProducts } from "./generatedCatalog.js";
+import { normalizeProduct, sortProductsByCommercialOrder } from "../features/products/productUtils.js";
 
-export const products = generatedProducts;
+export const products = sortProductsByCommercialOrder(generatedProducts.map(normalizeProduct));
